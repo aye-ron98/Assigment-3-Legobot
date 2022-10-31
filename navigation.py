8fecbb8b86b1099f17a63ba9b51f784afc04098d
aye-ron98 <aron.zhang013@gmail.com>
2022-10-31 12:20:29 -0700

    First edit

--- navigation.py
+++ navigation.py
@@ -1,6 +1,6 @@
 from mindstorms import MSHub, MotorPair, ColorSensor
 
-motor_pair = MotorPair('A', 'B')
+motor_pair = MotorPair('B', 'A')
 color_sensor = ColorSensor('C')
 
 
@@ -12,38 +12,23 @@ def course_correction():
     :return: an integer
     """
     global color_sensor
-    porportinal_error = 2500 - color_sensor.get_reflected_light()  # arbitrary placeholder
+    porportinal_error = 9 - color_sensor.get_reflected_light()  # arbitrary placeholder
     integral_error = porportinal_error - color_sensor.get_reflected_light()
     derived_error = porportinal_error - color_sensor.get_reflected_light()
 
     return 5 * porportinal_error + 5 * integral_error + 5 * derived_error
 
 
-def navigate_car():
-    """
-    Drive car
-    :return: position
-    """
-    global motor_pair
-    global color_sensor
-
-    while True:
-        intensity = color_sensor.get_reflected_light()
-        if intensity == 2500:
-            motor_pair.start(30 - course_correction(), 25 + course_correction())
-            print('black')
-        elif intensity != 2500:
-            motor_pair.start(0, 25 + course_correction())
-            print('white')
-        elif intensity < 30:
-            motor_pair.stop()
-            print('end')
-
 while True:
-    color = color_sensor.wait_for_new_color()
-    if color == 'black':
-        motor_pair.start(0, 30)
+    intensity = color_sensor.get_reflected_light()
+    if intensity < 65:
+        motor_pair.start(-10, -8)
         print('black')
-    elif color == 'white':
-        motor_pair.stop()
+
+    elif 72 < intensity < 90:
+        motor_pair.start(0, -5)
         print('white')
+
+    elif intensity > 90:
+        motor_pair.stop()
+        print('end')
