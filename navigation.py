022a5abae3bd004bb03b8760a198a05930a43e2a
aye-ron98 <aron.zhang013@gmail.com>
2022-11-10 20:15:37 -0800

    refactor navigation.py

--- navigation.py
+++ navigation.py
@@ -1,13 +1,36 @@
-
-from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, Appfrom mindstorms.control import wait_for_seconds, wait_until, Timerfrom mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_toimport math  # Initialize the hub.hub = MSHub()# Initialize the motors connected to Ports A and B.motor_pair = MotorPair('A', 'B')# Initialize the Color Sensor connected to Port C.color_sensor = ColorSensor('C')  def course_correction(light_offset, correction_value, multiplier):    """    Calculate course correction scalar.    Function to examine the difference between centerline black and current position. Will return an integer to be used    to scale motor speed.    :param light_offset: an integer [0, 100]    :precondition: light_offset must be an integer [1, 100]    :postcondition: will calculate the difference between 65 and light_offset    :return: the difference between 65 and light_offset as an integer    """        proportional_error = light_offset - correction_value    return multiplier * proportional_error  def drive(speed, correction_value, multiplier):    """    Drives robot.    A function to drive the robot on the black line    :param speed: integer between [1, 100]    :precondition: speed must be an integer between [1, 100]    :postconidion: wheel motors will move at rotation rate equivalent to speed    """
-from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
-from mindstorms.control import wait_for_seconds, wait_until, Timer
-from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
-import math
-
+from mindstorms import MSHub, MotorPair, ColorSensor, App
 
 # Initialize the hub.
 hub = MSHub()
 # Initialize the motors connected to Ports A and B.
 motor_pair = MotorPair('A', 'B')
+# Initialize the Color Sensor connected to Port C.
+color_sensor = ColorSensor('C')
 
+
+def drive(speed, target_value, multiplier):
+    """
+    Drives robot.
+    A function to drive the robot on the black line
+
+    :param speed: integer between [1, 100]
+    :param target_value: integer value [0, 100]
+    :param multiplier: integer value, this value will control steering,
+    :precondition: recommend set speed to 25
+    :precondition: set target_value to edge between black tape and off course
+    :precondition: recommend set multiplier to 8, lower will decrease steering higher increases
+    :precondition: align robot to right edge of tape
+    :postcondition: robot will follow the black line
+    """
+
+    while True:
+        intensity = color_sensor.get_reflected_light()
+        steer = intensity - target_value * multiplier
+
+        motor_pair.set_default_speed(speed)
+        motor_pair.start(steer)
+
+        print(steer)
+
+
+drive(15, 25, 8)
